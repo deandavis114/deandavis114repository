b5eb5b32405442b5356391a9a3363d81c74f2504
Dean E. Davis <dean.e.davis@Deans-MacBook-Air.local>
2020-10-09 19:27:30 -0600

    lesson 4 nav now works\

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,4 @@
-function toggleMenu () {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+function toggleMenu() {
+    
+    document.getElementById("primarynav").classList.toggle("hide");
 }
