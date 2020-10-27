0d2f29d4858ea3841361e7dc1aeebb791590412d
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-10-27 14:47:49 -0600

    sl

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,24 +1,7 @@
-document.addEventListener("DOMcontentLoaded", function(){
+let temp = document.getElementById("temp").innerText;
 
-    let speed = 25;
-    let temp = 10;
-    buildWC(speed, temp);
-});
+let speed = document.getElementById("speed").innerText;
 
+let wc = 35.74 + 0.6215 * temp -35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed,0.16);
 
-function buildWC(speed, tem) {
-
-    let feelTemp=document.getElementById('feeltemp');
-
-let wc = 35.74 + 0.6215 * temp -35.75 * 
-Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed,0.16);
-
-wc = math.floor(wc);
-
-wc = (wc > temp) ? temp : wc;
-
-
-console.log(wc);
-
-feelTemp.innerHTML = wc;
-}
+console.log(wc)
