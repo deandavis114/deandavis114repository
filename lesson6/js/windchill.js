dc9fb1dce22efdadf5689bca772b5c7633516374
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-10-27 14:51:09 -0600

    oct 27 2:51 pm

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,3 +5,5 @@ let speed = document.getElementById("speed").innerText;
 let wc = 35.74 + 0.6215 * temp -35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed,0.16);
 
 console.log(wc)
+
+document.getElementById("windchill").innerText = wc;
