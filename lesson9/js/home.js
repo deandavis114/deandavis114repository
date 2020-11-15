4a5bb6e6cd707bd215da17a62e8b30ee5ddc5787
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-11-14 23:16:59 -0700

    Nov 14 11:15PM

---
+++ lesson9/js/home.js
@@ -0,0 +1,37 @@
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'
+fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(jsonObject){
+        console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            let p1 = document.createElement("p1");
+            let p2 = document.createElement("p2");
+            let p3 = document.createElement("p3");
+            let img = document.createElement ("img");
+           
+            
+            h2.textContent = towns[i].name;
+            p.textContent = 'moto'+ ':'+ ' ' + towns[i].motto;
+            p1.textContent = 'Year Founded' + ' ' + towns[i].yearFounded;
+            p2.textContent = 'population' + ' ' + towns[i].currentPopulation;
+            p3.textContent = 'Annual Rain Fall' +' ' + towns[i].averageRainfall;
+            img.setAttribute('src',towns[i].imageurl);
+
+            card.appendChild(h2);
+            card.appendChild(p);
+            card.appendChild(p1);
+            card.appendChild(p2);
+            card.appendChild(p3);
+            card.appendChild(img);
+
+
+            document.querySelector('div#data1').appendChild(card);
+        }
+    });
