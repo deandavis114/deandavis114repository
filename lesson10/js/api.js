0e8501ceb6bc12db69ee07890ac4759d6b6d79b4
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-11-20 18:49:19 -0700

    dfsjo;i

--- lesson10/js/api.js
+++ lesson10/js/api.js
@@ -1,5 +1,6 @@
 const apiKey  = "de8e234434351321aacbcad506c213dd";
-let url       = "http://api.openweathermap.org/data/2.5/weather";
+let url       = "http://api.openweathermap.org/data/2.5/";
+let method    = "weather";
 let cityId    = "5604473";
 let units     = "imperial";
 
@@ -7,7 +8,7 @@ let units     = "imperial";
 
 "http://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}"
 
-let requestUrl = url + "?" + 
+let requestUrl = url + method + "?" + 
     "id=" + cityId +
     "&appid=" + apiKey +
     "&units=" + units;
@@ -40,3 +41,37 @@ fetch(requestUrl)
         console.log(imageSrcUrl);
         document.getElementById("icon").setAttribute('src', imageSrcUrl);
     });
+
+
+
+//Five day forecast
+   
+method = "forecast"
+requestUrl = url + method + "?" + 
+    "id=" + cityId +
+    "&appid=" + apiKey +
+    "&units=" + units;
+
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+    .then( response => response.json())
+    .then( response =>{
+
+        //interprete forecast
+
+        let forecast_temps = document.querySelectorAll('.forecast-day .temp');
+        let forecast_dow = document.querySelectorAll('.forecast-day .dow');
+
+        let list = response.list;
+
+        for (item of list){
+            if (item.dt_txt.includes('18:00:00')){
+                let date = new Date(item.dt * 1000);
+
+
+               
+    }
+}
+
+});
