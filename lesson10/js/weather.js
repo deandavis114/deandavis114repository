bc2f39a818fdcfe0fd72c899176cc50d4e93b597
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-11-21 22:08:21 -0700

    Nov 21 10:08pm

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -21,11 +21,58 @@ fetch(requestUrl)
     .then(data => {
         // Parse the JASON stuff
 
-        console.log(data);
-        console.log(data.weather[0].description);
+       
+        let real_description = data.list[1].weather[0].description;
+        document.getElementById('description').innerText = real_description;
 
-        let temp = data.main.temp;
-        document.getElementById('current-temp').innerText = temp;
+        let real_high = data.list[0].main.temp_max;
+        document.getElementById('temp_high').innerText = real_high;
 
-        
+        let real_windchill = data.list[3].wind.deg;
+        document.getElementById('windchill_now').innerText = real_windchill;
+       
+        let real_humidity = data.list[0].main.humidity;
+        document.getElementById('humidity_now').innerText = real_humidity;
+       
+
+        let real_wind_speed = data.list[3].wind.speed;
+        document.getElementById('speed_now').innerText = real_wind_speed;
+
+        console.log('data');
+
+
+        //5 day forecast//
+        let forecast_temps = document.querySelectorAll('.forecast-day .temp');
+        let forecast_dow = document.querySelectorAll('.forecast-day .dow');
+        let list = data.list;
+        const DOW = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let count = 5 
+        for(item of list){
+            if(item.dt_txt.includes('18:00:00')){
+                let date = new Date(item.dt * 1000);
+                if (count == 5) {
+                    document.getElementById('DayOfWeek1').innerText = DOW[date.getDay()];
+                }
+                if (count == 4) {
+                    document.getElementById('DayOfWeek2').innerText = DOW[date.getDay()];
+                }
+                if (count == 3) {
+                    document.getElementById('DayOfWeek3').innerText = DOW[date.getDay()];
+                }
+                if (count == 2) {
+                    document.getElementById('DayOfWeek4').innerText = DOW[date.getDay()];
+                }
+                if (count == 1) {
+                    document.getElementById('DayOfWeek5').innerText = DOW[date.getDay()];
+                }
+                //date.getDay
+                count = count-1
+            }
+        }
+
+
+
+
+
+
     });
