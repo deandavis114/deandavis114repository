ccae9ecd079e2fb07bbeb4bee8039aa15f1c3103
Dean E. Davis <dean.e.davis@205-185-99-116.directcom.com>
2020-11-30 18:04:09 -0700

    sfkjdn

---
+++ lesson11/js/events.js
@@ -0,0 +1,25 @@
+const townevent = document.getElementById('eventsection')
+const cityName = document.getElementById('cityname')
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        let towns = jsonObject.towns
+        console.log(towns)
+        console.log(cityname)
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == cityname) {
+                let events = towns[i].events;
+                // townevent.innerHTML = events;
+                let h = '<ul>';
+                for (event of events) {
+                    h += `<li>${event}</li>`;
+                }
+                h += '</ul>';
+                townevent.innerHTML = h;
+                break; // We don't need to keep looping through the list of remaining towns
+            }
+        }
+    });
