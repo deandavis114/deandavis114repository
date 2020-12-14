e6c89fbb0864e08654132347578998f1bf23f8b0
Dean Davis <deandavis114@gmail.com>
2020-12-14 10:54:59 -0700

    skldjf

---
+++ lesson12final/js/navmenu.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    
+  document.getElementById("primarynav").classList.toggle("hide");
+}
+
+/* IIFE to avoid collisions with global namespace */
+(function () {
+
+  /* Hamburger Menu */
+  const menu = document.querySelectorAll(".navigation")[0];
+  document.getElementById("menu-button").addEventListener("click", () => {
+    menu.classList.toggle("responsive");
+  });
+
+
+});
+
+// (function () {
+//   /* Hamburger Menu */
+//   const menu = document.querySelectorAll(".navigation")[0];
+//   document.getElementById("menu-button").addEventListener("click", () => {
+//      menu.classList.toggle("responsive");
+//   });
+// })();
